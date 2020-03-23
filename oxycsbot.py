ac35b9e8781c2208f50bf188f7982588bc14027e
Sara <sara.bawale@gmail.com>
2020-03-22 18:49:20 -0700

    Update oxycsbot.py

--- oxycsbot.py
+++ oxycsbot.py
@@ -181,42 +181,41 @@ class OxyCSBot(ChatBot):
             str: The message to send to the user.
         """
 
-        # if 'pro_vegan_stance' in tags or 'anti_vegan_stance' in tags or 'veganism' in tags:
+        if 'pro_vegan_stance' in tags or 'anti_vegan_stance' in tags or 'veganism' in tags:
             # Use tags and message to determine user stance, then define bot's stance as the opposite
-        # If user is neutral/has no opinion, the bot will randomly choose between pro and con
-        if self.stance == None and ('pro_vegan_stance' in tags or 'anti_vegan_stance' in tags or 'veganism' in tags):
+            # If user is neutral/has no opinion, the bot will randomly choose between pro and con
+            if self.stance == None:
 
-            # If user is pro-vegan, bot takes anti-vegan stance
-            if 'pro_vegan_stance' in tags:
-                self.stance = 'anti_vegan'
-                return self.go_to_state('anti_vegan_stance')
-
-            # If user is anti-vegan, bot takes pro-vegan stance
-            elif 'anti_vegan_stance' in tags:
-                self.stance = 'pro_vegan'
-                return self.go_to_state('pro_vegan_stance')
-
-            # If user is neutral, bot chooses randomly between pro and anti vegan stances
-            elif 'veganism' in tags:
-                # return self.finish('neutral')
-                self.stance = random.choice(STANCES)
-
-                if self.stance == 'pro_vegan':
-                    return self.go_to_state('pro_vegan_stance')
-                else:
+                # If user is pro-vegan, bot takes anti-vegan stance
+                if 'pro_vegan_stance' in tags:
+                    self.stance = 'anti_vegan'
                     return self.go_to_state('anti_vegan_stance')
+
+                # If user is anti-vegan, bot takes pro-vegan stance
+                elif 'anti_vegan_stance' in tags:
+                    self.stance = 'pro_vegan'
+                    return self.go_to_state('pro_vegan_stance')
+
+                # If user is neutral, bot chooses randomly between pro and anti vegan stances
+                elif 'veganism' in tags:
+                    # return self.finish('neutral')
+                    self.stance = random.choice(STANCES)
+
+                    if self.stance == 'pro_vegan':
+                        return self.go_to_state('pro_vegan_stance')
+                    else:
+                        return self.go_to_state('anti_vegan_stance')
+                else:
+                    return self.finish('neutral')
+
+            # If bot already has a stance, return to the response state for that stance
             else:
-                return self.finish('neutral')
+                if self.stance == 'anti_vegan':
+                    return self.go_to_state('anti_vegan_stance')
+                else:
+                    return self.go_to_state('pro_vegan_stance')
 
-        # If bot already has a stance, return to the response state for that stance
-        elif self.stance == 'pro_vegan' or self.stance == 'anti_vegan':
-            if self.stance == 'anti_vegan':
-                return self.go_to_state('anti_vegan_stance')
-            else:
-                return self.go_to_state('pro_vegan_stance')
-
-
-        elif 'reset' in tags:
+        elif 'reset' in message:
             self.stance = None
             self.used_arguments = []
             return "Stance and used arguments cleared."
